a0f47b2e8c2d2fadf94c0609cc27208b4ef94eff
vishwajitshinde2101 <vishwajitshinde2101@gmail.com>
2025-04-24 14:38:16 +0530

    Remove unused captcha download function
    Eliminated the redundant `downloadCaptcha` function as it is no longer utilized in the codebase. This cleanup improves maintainability and reduces unnecessary complexity in the file.

--- freesearchigrservice.js
+++ freesearchigrservice.js
@@ -23,35 +23,6 @@ async function getPlots() {
 }
 
 
-async function downloadCaptcha() {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-    const CAPTCHA_URL = 'https://freesearchigrservice.maharashtra.gov.in/Handler.ashx?txt=8c779b';
-    const headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,mr;q=0.7',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'Cookie': 'ASP.NET_SessionId=App5-44~dfo0uh30lajxc5ts2lbvfk2x',
-    };
-
-    try {
-        const response = await axios.get(CAPTCHA_URL, {headers, responseType: 'arraybuffer'});
-        fs.writeFileSync(CAPTCHA_IMAGE_PATH, response.data);
-        console.log('✅ Captcha image downloaded');
-    } catch (err) {
-        console.error('❌ Failed to download captcha:', err.message);
-    }
-}
-
 async function handleCaptcha(page) {
     await page.waitForSelector(captchaSelector);
     const captchaElement = await page.$(captchaSelector);
